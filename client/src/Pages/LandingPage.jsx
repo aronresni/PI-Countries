139fe3b05e4a06ba1812d8ef4b03a79c92d0dc3a
aronresni <aronresni@gmail.com>
2023-09-14 15:38:55 -0300

    upload front

---
+++ client/src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className='container'>
+            <div>
+                <button>START</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default LandingPage
