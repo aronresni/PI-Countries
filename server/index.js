69886b569633ec74c436694bfb2d04497712b440
aronresni <aronresni@gmail.com>
2023-10-05 17:44:10 -0300

    upload

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const {
+  PORT
+} = process.env;
 
 conn.sync({ force: true }).then(() => {
 server.listen(PORT, () => {
